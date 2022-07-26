bccdb847e29372c3c8d58169d9f90e8882f15c73
Daniel Nichols <dando18studios@gmail.com>
2022-07-26 12:50:15 -0400

    refactor build functions

--- software-search/software_search/runner.py
+++ software-search/software_search/runner.py
@@ -23,20 +23,46 @@ class BashRunner():
         self.num_ranks = num_ranks
         self.mpi_cmd = run_cmd
 
+
+    def get_build_commands(self, software, compiler, dependencies):
+        spack_env_str = ''
+        if self.spack_env:
+            spack_env_str = '-e {} '.format(self.spack_env)
+        
+        build_commands_str = 'spack {}install {}'.format(
+            spack_env_str,
+            software.get_spack_spec(compiler=compiler, dependencies=software)
+        )
+
+        self.set_spack_load(software.get_spack_spec(compiler=compiler, dependencies=dependencies))
+
+        return [build_commands_str]
+    
+    def build(self, software, compiler, dependencies):
+        commands = self.get_build_commands(software, compiler, dependencies)
+        self.set_spack_load(software.get_spack_spec(compiler=compiler, dependencies=dependencies))
+        
+        # run commands
+        for cmd in commands:
+            res = subprocess.run(cmd.split(' '))
+            if res.returncode != 0:
+                return
+
+
     def get_commands(self, software):
         commands = []
 
         spack_env_str = ''
         if self.spack_env:
-            commands.extend([
-                'spack env deactivate',
-                'spack env activate {}'.format(self.spack_env)
-            ])
-            spack_env_str = '-e {}'.format(self.spack_env)
+            #commands.extend([
+            #    'spack env deactivate',
+            #    'spack env activate {}'.format(self.spack_env)
+            #])
+            spack_env_str = '-e {} '.format(self.spack_env)
         
         for spec in self.spack_loads:
             load_commands_str = map(
-                    lambda x: 'spack {} load {}'.format(spack_env_str, x), 
+                    lambda x: 'spack {}load {}'.format(spack_env_str, x), 
                     self.spack_loads
                 )
             commands.extend(load_commands_str)
@@ -47,6 +73,7 @@ class BashRunner():
         
         commands.append(command_str)
         return commands
+        
 
     def run(self, software):
         commands = self.get_commands(software)
--- software-search/software_search/software_search.py
+++ software-search/software_search/software_search.py
@@ -3,20 +3,17 @@ author: Daniel Nichols
 date: August 2021
 '''
 
-def build_spec(spec_str, spack_env=None, dry=False):
-    commands = []
+def build(software, compiler, dependencies, runner, spack_env=None, dry=False):
 
-    spack_env_str = ''
     if spack_env:
-        spack_env_str = '-e {}'.format(spack_env)
-
-    commands.append('spack {} install {}'.format(spack_env_str, spec_str))
+        runner.set_spack_env(spack_env)
 
     if dry:
-        print('\n'.join(commands))
+        print('\n'.join(runner.get_build_commands(software, compiler, dependencies)))
         return
     
     # run commands
+    runner.build(software, compiler, dependencies)
 
 
 def run_and_profile(software, runner, spack_env=None, dry=False):
@@ -41,11 +38,8 @@ def search(search_strategy, runner, main_software, compilers, dependencies, dry=
 
     for compiler, *software in search_strategy.search_space():
         # BUILD
-        build_spec_str = main_software.get_spack_spec(compiler=compiler, dependencies=software)
         if do_build:
-            build_spec(build_spec_str, spack_env=spack_env, dry=dry)
-
-            runner.set_spack_load(build_spec_str)
+            build(main_software, compiler, software, runner, spack_env=spack_env, dry=dry)
 
         # RUN
         if do_run:
